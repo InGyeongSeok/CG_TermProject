7177b466daa5279ab2a082c28bf432a83d656689
Kim-yunsoo <dev.yunsoo@gmail.com>
2022-11-29 05:26:06 +0900

    .

--- CG_TermProject/CG_TermProject/Camera.cpp
+++ CG_TermProject/CG_TermProject/Camera.cpp
@@ -11,22 +11,22 @@ float VAngleX;
 float VAngleY;
 
 void camera() {
-	glm::vec3 CPos(carAddX+1, carAddY-0.5 , carAddZ + 10);
-	glm::vec3 CDir(1.f + carAddX, 0.0f + carAddY, 0.0f + carAddZ);
+	glm::vec3 CPos(carAddX+1, -0.5+carAddY , carAddZ + 10);
+	glm::vec3 CDir(1.f + carAddX, -0.5f+  carAddY, 0.0f + carAddZ);
 
-	glm::mat4 VAngleY_Rot = glm::rotate(glm::mat4(1.0f), glm::radians(VAngleY), glm::vec3(0.0, 1.0, 0.0)); //y?
-	glm::mat4 VAngleX_Rot = glm::rotate(glm::mat4(1.0f), glm::radians(VAngleX), glm::vec3(1.0, 0.0, 0.0)); // x
+	glm::mat4 VAngleY_Rot = glm::rotate(glm::mat4(1.0f), glm::radians(VAngleY), glm::vec3(0.0, 1.0, 0.0)); 
+	glm::mat4 VAngleX_Rot = glm::rotate(glm::mat4(1.0f), glm::radians(VAngleX), glm::vec3(1.0, 0.0, 0.0)); 
 	
-	glm::vec3 cameraPos = glm::vec3(CPos.x, CPos.y, CPos.z);      //--- ī�޶� ��ġ (��� ������)
-	glm::vec3 cameraDirection = glm::vec3(CDir.x, CDir.y, CDir.z);               //--- ī�޶� �ٶ󺸴� ���� (��𺼰��� �ϸ�ɵ�)
-	glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);                     //--- ī�޶� ���� ����->������(���⸸) (�����ϸ� ȭ�� ���ϰŲٷκ���)
+	glm::vec3 cameraPos = glm::vec3(CPos.x, CPos.y, CPos.z);      
+	glm::vec3 cameraDirection = glm::vec3(CDir.x, CDir.y, CDir.z);              
+	glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);                    
 
 	glm::mat4 view = VAngleX_Rot * VAngleY_Rot * glm::lookAt(cameraPos, cameraDirection, cameraUp);
 
-	unsigned int viewLocation = glGetUniformLocation(shaderID, "viewTransform"); //--- ���� ��ȯ ����
+	unsigned int viewLocation = glGetUniformLocation(shaderID, "viewTransform"); 
 	glUniformMatrix4fv(viewLocation, 1, GL_FALSE, &view[0][0]);
 
-	unsigned int viewPosLocation = glGetUniformLocation(shaderID, "viewPos");      //--- viewPos �� ����: ī�޶� ��ġ
+	unsigned int viewPosLocation = glGetUniformLocation(shaderID, "viewPos");      
 	glUniform3f(viewPosLocation, cameraPos.x, cameraPos.y, cameraPos.z);
 
 
