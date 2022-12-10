0e2acded6b2a473d0c2f8583890bfc02661ec670
Kim-yunsoo <dev.yunsoo@gmail.com>
2022-12-10 23:40:06 +0900

    총소리 수정 완료

--- CG_TermProject/CG_TermProject/Mouse.cpp
+++ CG_TermProject/CG_TermProject/Mouse.cpp
@@ -6,8 +6,8 @@ GLvoid Mouse(int button, int state, int x, int y) {
 	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
 		if (!herodead) {
 			isBullet = true;
+			PlaySound(L"최종총소리.wav", 0, SND_FILENAME | SND_ASYNC); //일반 재생
 		}
-		PlaySound(L"최종총소리.wav", 0, SND_FILENAME | SND_ASYNC); //일반 재생
 	}
 	if (button == GLUT_LEFT_BUTTON && state == GLUT_UP) {
 		BulletLimit = 0;
